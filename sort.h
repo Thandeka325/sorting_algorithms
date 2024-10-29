a7491b3710283fb6ad6203891c098c757e7f23e0
Thandeka325 <marhwayiza325@gmail.com>
2024-10-29 14:10:31 -0700

    Project: 0x1B. C - Sorting algorithms & Big O

--- sort.h
+++ sort.h
@@ -1,10 +1,23 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stdio.h>
 #include <stdlib.h>
 
+/* Comparison direction macros for bitonic sort */
+#define UP 0
+#define DOWN 1
 
-/** Data Structure **/
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
 
 /**
  * struct listint_s - Doubly linked list node
@@ -15,19 +28,16 @@
  */
 typedef struct listint_s
 {
-       	const int n;
+	const int n;
 	struct listint_s *prev;
-       	struct listint_s *next;
+	struct listint_s *next;
 } listint_t;
 
-
-/** Functions **/
-
+/* Functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-void swap_nodes(listint_t **list, listint_t *left, listint_t *right);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
@@ -38,7 +48,5 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-/**void sort_deck(deck_node_t **deck);**/
 
-
-#endif /** SORT_H **/
+#endif /* SORT_H */
